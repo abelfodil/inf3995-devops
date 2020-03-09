d12130581b2d1bbfb50d6bb61635ad6e36476aa4
Anes Belfodil <ans.belfodil@gmail.com>
2020-03-08 22:19:53 -0400

    Remove submodules and move to FetchContent

--- 2-cmake/compile.sh
+++ 2-cmake/compile.sh
@@ -4,4 +4,4 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 BUILD_DIR="$SOURCE_DIR/cmake-build"
 
 NINJA=$([ -x "$(command -v ninja)" ] && echo "-GNinja")
-cmake -H"$SOURCE_DIR" -B"$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release $NINJA && cmake --build "$BUILD_DIR" --target $1
+cmake -H"$SOURCE_DIR" -B"$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release $NINJA && cmake --build "$BUILD_DIR" --target $1 -- -j $(nproc)
